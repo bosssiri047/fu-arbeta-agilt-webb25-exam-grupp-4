2d689c7f774905d422376465578f10934c3fa560
Boss <chaiyapat_ass@hotmail.com>
2026-03-09 10:41:37 +0100

    initial setup

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+if(window.location.pathname === '/' || window.location.pathname.includes ('index.html')) {
+    console.log('index.html');
+    pageSetup();
+
+} else if(window.location.pathname.includes('foodtruck.html')) {
+    console.log('foodtruck.html');
+    foodtruckSetup();
+
+} else if(window.location.pathname.includes('menu.html')) {
+    console.log('menu.html');
+    menuSetup();
+
+} else if(window.location.pathname.includes('cart.html')) {
+    console.log('cart.html');
+    cartSetup();
+
+} else if(window.location.pathname.includes('receipt.html')) {
+    console.log('receipt.html');
+    receiptSetup();
+}
+
+function pageSetup () {
+
+}
+
+function foodtruckSetup () {
+    
+}
+
+async function menuSetup () {
+    
+}
+
+function cartSetup () {
+    
+}
+
+function receiptSetup () {
+    
+}
