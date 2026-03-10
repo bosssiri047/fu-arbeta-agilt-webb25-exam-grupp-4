c3aa195ea335807f72c55af5d7f48f0b1e86822a
vincent <vincent.samuelsson@hotmail.com>
2026-03-10 14:59:06 +0100

    landing page first

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,77 @@
-if(window.location.pathname === '/' || window.location.pathname.includes ('index.html')) {
-    console.log('index.html');
-    pageSetup();
+import { fetchProducts } from "./modules/api.js";
+import { renderHamburgerMenu, renderProducts } from "./modules/gui.js";
+import { addToCart } from "./modules/localeStroage.js";
+import { getElementAll } from "./utils/domutils.js";
 
-} else if(window.location.pathname.includes('foodtruck.html')) {
-    console.log('foodtruck.html');
-    foodtruckSetup();
-
-} else if(window.location.pathname.includes('menu.html')) {
-    console.log('menu.html');
-    menuSetup();
-
-} else if(window.location.pathname.includes('cart.html')) {
-    console.log('cart.html');
-    cartSetup();
-
-} else if(window.location.pathname.includes('receipt.html')) {
-    console.log('receipt.html');
-    receiptSetup();
+if (
+  window.location.pathname === "/" ||
+  window.location.pathname.includes("index.html")
+) {
+  console.log("index.html");
+  pageSetup();
+} else if (window.location.pathname.includes("foodtruck.html")) {
+  console.log("foodtruck.html");
+  foodtruckSetup();
+} else if (window.location.pathname.includes("menu.html")) {
+  console.log("menu.html");
+  menuSetup();
+} else if (window.location.pathname.includes("cart.html")) {
+  console.log("cart.html");
+  cartSetup();
+} else if (window.location.pathname.includes("receipt.html")) {
+  console.log("receipt.html");
+  receiptSetup();
 }
 
-function pageSetup () {
-
+function pageSetup() {
+  renderHamburgerMenu();
+}
+function foodtruckSetup() {
+  renderHamburgerMenu();
+  loadFoodtruckEventListeners();
 }
 
-function foodtruckSetup () {
-    
+async function menuSetup() {
+  renderHamburgerMenu();
+  const products = await fetchProducts();
+  console.log(products);
+  renderProducts(products);
+
+  const menuRef = getElementAll(".menu__list-item");
+  console.log(menuRef);
+
+  for (let list of menuRef) {
+    list.addEventListener("click", (event) => {
+      addToCart(list.id);
+    });
+  }
 }
 
-async function menuSetup () {
-    
+function cartSetup() {
+  renderHamburgerMenu();
 }
 
-function cartSetup () {
-    
+function receiptSetup() {
+  renderHamburgerMenu();
 }
 
-function receiptSetup () {
-    
+// EVENT LISTENERS
+
+// FOODTRUCK PAGE
+function loadFoodtruckEventListeners() {
+  document
+    .querySelector("#foodTruckContainer")
+    .addEventListener("click", (event) => {
+      const click = event.target;
+
+      if (click.closest("#foodTruck1")) {
+        console.log("foodtruck1");
+      } else if (click.closest("#foodTruck2")) {
+        console.log("foodtruck2");
+      } else if (click.closest("#foodTruck3")) {
+        console.log("foodtruck3");
+      } else if (click.closest("#foodTruck4")) {
+        console.log("foodtruck4");
+      }
+    });
 }
